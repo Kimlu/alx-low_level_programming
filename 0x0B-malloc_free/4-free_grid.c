e63faebc3617729405e344e4104f835d73c9dd40
Kimlu <wilojei@gmail.com>
2022-10-05 13:15:34 -0700

    malloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_grid - freezes 2d atrray
+ * @grid: memory block to be freed
+ * @height: height of array
+ * Return: returns void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free[grid];
+}
